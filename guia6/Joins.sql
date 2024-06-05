d70306d82cf67cb980288f827f88bbb2fa66d847
mrsMendez <161641451+mrsMendez@users.noreply.github.com>
2024-06-05 07:34:14 -0600

    agregacion y modificacion

--- guia6/Joins.sql
+++ guia6/Joins.sql
@@ -6,33 +6,43 @@
 -- el vendedor, y el monto de la venta incluyendo impuestos fiscales como el IVA y
 -- crédito fiscal (si está registrando este último).
 use supermarket;
-
-SELECT 
-    p.producto, 
-    c.categoria,
-    m.marca,
-    u.unidadMedida,
-    d.distribuidor
+-- 1
+SELECT p.producto, c.categoria, m.marca, u.unidadMedida, d.distribuidor
 FROM producto p
-    INNER JOIN categoria c ON p.idCategoria = c.idCategoria
-    INNER JOIN marca m ON p.idMarca = m.idMarca
-    INNER JOIN unidadMedida u ON p.idUnidadMedida = u.idUnidadMedida
-    INNER JOIN distribuidor d ON p.idDistribuidor = d.idDistribuidor;
+INNER JOIN categoria c ON p.idCategoria = c.idCategoria
+INNER JOIN marca m ON p.idMarca = m.idMarca
+INNER JOIN unidadmedida u ON p.idUnidadMedida = u.idUnidadMedida
+INNER JOIN distribuidor d ON p.idDistribuidor = d.idDistribuidor;
 
 -- 2
+SELECT * FROM venta;
+SELECT * FROM detalleVenta;
+SELECT * FROM producto;
+SELECT * FROM cliente;
+SELECT * FROM empleado;
+SELECT * FROM facturaVenta;
+
 SELECT
   v.fechaVenta AS Fecha_Venta,
   CONCAT(c.nombreCliente, ' ', c.apellidoCliente) AS Cliente,
   CONCAT(e.nombreEmpleado, ' ', e.apellidoEmpleado) AS Empleado,
   dv.cantidadProductosVenta AS Cantidad,
+  p.producto AS producto,
   p.precioVenta AS Precio_Unitario,
   (dv.cantidadProductosVenta * p.precioVenta) AS SubTotal,
   fv.ivaFacturaVenta AS IVA,
   fv.creditoFiscalFacturaVenta AS Credito_Fiscal,
   (dv.cantidadProductosVenta * p.precioVenta) + fv.ivaFacturaVenta AS Total
-FROM venta v
-    INNER JOIN detalleVenta dv ON v.idVenta = dv.idVenta
-    INNER JOIN producto p ON dv.idProducto = p.idProducto
-    INNER JOIN cliente c ON v.idCliente = c.idCliente
-    INNER JOIN empleado e ON v.idEmpleado = e.idEmpleado
-    INNER JOIN facturaVenta fv ON dv.idDetalleVenta = fv.idDetalleVenta;
+FROM 
+  venta v
+INNER JOIN 
+  detalleventa dv ON v.idVenta = dv.idVenta
+INNER JOIN 
+  producto p ON dv.idProducto = p.idProducto
+INNER JOIN 
+  cliente c ON v.idCliente = c.idCliente
+INNER JOIN 
+  empleado e ON v.idEmpleado = e.idEmpleado
+INNER JOIN 
+  facturaventa fv ON dv.idDetalleVenta = fv.idDetalleVenta;
+
